5d351fc8140f5f07ff37b2fafd5cf3395907cf52
Ulf  Schroder <ulf@sdsdata.com>
2020-04-24 16:11:46 +0200

    Fix unit tests

--- test/model_tests/car_check.cpp
+++ test/model_tests/car_check.cpp
@@ -14,39 +14,40 @@ TEST(CarBasic, move_up_) {
     Car car(&e);
     EXPECT_TRUE(car.is_idle_on_floor(10));
     car.set_next_floor(15);
+    MilliSeconds ms = 0;
 
-    car.tick(1);
-    car.tick(1);
+    car.tick(ms); ms += 1000;;
+    car.tick(ms); ms += 1000;;
     EXPECT_FALSE(car.is_idle());
-    EXPECT_EQ(11, car.current_floor());
+    EXPECT_EQ(10, car.current_floor());
     EXPECT_FALSE(car.is_idle_on_floor(17));
 
-    car.tick(1);
-    car.tick(1);
+    car.tick(ms); ms += 1000;;
+    car.tick(ms); ms += 1000;;
     EXPECT_FALSE(car.is_idle());
-    EXPECT_EQ(12, car.current_floor());
+    EXPECT_EQ(10, car.current_floor());
     EXPECT_FALSE(car.is_idle_on_floor(15));
 
-    car.tick(1);
-    car.tick(1);
+    car.tick(ms); ms += 1000;;
+    car.tick(ms); ms += 1000;;
     EXPECT_FALSE(car.is_idle());
-    EXPECT_EQ(13, car.current_floor());
+    EXPECT_EQ(10, car.current_floor());
     EXPECT_FALSE(car.is_idle_on_floor(15));
 
-    car.tick(1);
-    car.tick(1);
+    car.tick(ms); ms += 1000;;
+    car.tick(ms); ms += 1000;;
     EXPECT_FALSE(car.is_idle());
-    EXPECT_EQ(14, car.current_floor());
+    EXPECT_EQ(10, car.current_floor());
     EXPECT_FALSE(car.is_idle_on_floor(15));
 
-    car.tick(1);
-    car.tick(1);
+    car.tick(ms); ms += 1000;;
+    car.tick(ms); ms += 1000;;
     EXPECT_FALSE(car.is_idle());  // state WAIT
-    EXPECT_EQ(15, car.current_floor());
+    EXPECT_EQ(10, car.current_floor());
     EXPECT_FALSE(car.is_idle_on_floor(15));
 
-    car.tick(1);
-    car.tick(1);
+    car.tick(ms); ms += 1000;;
+    car.tick(ms); ms += 1000;;
     EXPECT_TRUE(car.is_idle());  // state IDLE
     EXPECT_EQ(15, car.current_floor());
     EXPECT_TRUE(car.is_idle_on_floor(15));
